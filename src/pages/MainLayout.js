17de386ab84d859145e042d9a7d69643c314a334
손소희 <ssh30510044@gmail.com>
2022-10-19 15:47:48 +0900

    Add: header user image 추가

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -55,9 +55,24 @@ const MainLayout = () => {
         <Header
           className="site-layout-background"
           style={{
-            padding: 0,
+            // padding: 0,
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            boxShadow: '4px 3px 5px rgba(160, 160, 160, 0.2)',
           }}
-        />
+        >
+          <img
+            src="https://randomuser.me/api/portraits/women/62.jpg"
+            alt="user_image"
+            style={{
+              width: '50px',
+              height: '50px',
+              borderRadius: '50px',
+              border: '2px solid white',
+            }}
+          />
+        </Header>
         <Content
           style={{
             margin: '16px',
